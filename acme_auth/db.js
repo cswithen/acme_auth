bb821538d9badb8d42e7d2dafbd39b6bd596caf2
Matthew Baker <matthewbaker@MacBook-Pro.lan>
2021-05-24 13:09:17 -0400

    adding in bcrypt functionality

--- acme_auth/db.js
+++ acme_auth/db.js
@@ -1,77 +1,89 @@
-const Sequelize = require('sequelize');
+const Sequelize = require("sequelize");
 const { STRING } = Sequelize;
 const config = {
-  logging: false
+  logging: false,
 };
 
-const jwt = require('jsonwebtoken')
+const jwt = require("jsonwebtoken");
+const bcrypt = require('bcrypt')
 
-
-if(process.env.LOGGING){
+if (process.env.LOGGING) {
   delete config.logging;
 }
-const conn = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost/acme_db', config);
+const conn = new Sequelize(
+  process.env.DATABASE_URL || "postgres://localhost/acme_db",
+  config
+);
 
-const User = conn.define('user', {
+const User = conn.define("user", {
   username: STRING,
-  password: STRING
+  password: STRING,
 });
 
-User.byToken = async(token)=> {
+User.byToken = async (token) => {
   try {
-    const user = await jwt.verify(token, process.env.JWT)
-    if(user){
+    const user = await jwt.verify(token, process.env.JWT);
+    if (user) {
       return await User.findByPk(user.userId);
     }
-    const error = Error('bad credentials');
+    const error = Error("bad credentials");
     error.status = 401;
     throw error;
-  }
-  catch(ex){
-    const error = Error('bad credentials');
+  } catch (ex) {
+    const error = Error("bad credentials");
     error.status = 401;
     throw error;
   }
 };
 
-User.authenticate = async({ username, password })=> {
+User.authenticate = async ({ username, password }) => {
+  const hashedPass = await bcrypt.hash(password, 10)
+  // const auth = await bcrypt.compare(password, hashedPass)
+  // console.log(auth)
   const user = await User.findOne({
     where: {
       username,
-      password
-    }
+      password: hashedPass
+    },
   });
-  if(user){
-    const token = jwt.sign({ userId: user.id }, process.env.JWT)
-    return token
+  if (user) {
+
+    const token = jwt.sign({ userId: user.id }, process.env.JWT);
+    return token;
   }
-  const error = Error('bad credentials');
+  const error = Error("bad credentials");
   error.status = 401;
   throw error;
 };
 
-const syncAndSeed = async()=> {
+User.beforeCreate( async (user) => {
+  user.password = await bcrypt.hash(user.password, 10)
+  return user
+});
+
+const syncAndSeed = async () => {
   await conn.sync({ force: true });
   const credentials = [
-    { username: 'lucy', password: 'lucy_pw'},
-    { username: 'moe', password: 'moe_pw'},
-    { username: 'larry', password: 'larry_pw'}
+    { username: "lucy", password: "lucy_pw" },
+    { username: "moe", password: "moe_pw" },
+    { username: "larry", password: "larry_pw" },
   ];
   const [lucy, moe, larry] = await Promise.all(
-    credentials.map( credential => User.create(credential))
+    credentials.map((credential) => User.create(credential))
   );
+
   return {
     users: {
       lucy,
       moe,
-      larry
-    }
+      larry,
+    },
   };
 };
 
 module.exports = {
   syncAndSeed,
   models: {
-    User
-  }
+    User,
+  },
 };
